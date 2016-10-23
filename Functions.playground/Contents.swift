c804f705841ef0b07527a31fcb71b845717b4d59
Gvayn <mikhailpan@Mikhails-Air.home>
2016-10-23 15:48:29 -0400

    Complete Problems

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -10,6 +10,17 @@ sayHello()
 // (You can uncomment a line by removing the // at the beginning of the line.)
 //print(greeting)
 
+func sayHelloToGarfield() {
+        print("Hello, Garfield, why do you sleep so much?")
+    }
+
+func sayHelloToBuster() {
+        print("Hello, Buster, why do you sleep so much?")
+    }
+
+func sayHelloToScratchy() {
+        print("Hello, Scratchy, why do you sleep so much?")
+    }
 
 func sayHelloToCat(catName: String) {
     print("Hello, \(catName), why do you sleep so much?")
