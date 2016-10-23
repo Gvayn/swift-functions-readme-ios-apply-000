6188be503220326760cf08b09101000989d0bc7f
Gvayn <mikhailpan@Mikhails-Air.home>
2016-10-23 15:52:59 -0400

    Complete Problems

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -9,24 +9,19 @@ sayHello()
 // Uncomment this line to see the error that is printed in the console.
 // (You can uncomment a line by removing the // at the beginning of the line.)
 //print(greeting)
-
 func sayHelloToGarfield() {
-        print("Hello, Garfield, why do you sleep so much?")
-    }
-
+    print("Hello, Garfield, why do you sleep so much?")
+}
 func sayHelloToBuster() {
-        print("Hello, Buster, why do you sleep so much?")
-    }
-
+    print("Hello, Buster, why do you sleep so much?")
+}
 func sayHelloToScratchy() {
-        print("Hello, Scratchy, why do you sleep so much?")
-    }
-
+    print("Hello, Scratchy, why do you sleep so much?")
+}
 func sayHelloToCat(catName: String) {
     print("Hello, \(catName), why do you sleep so much?")
 }
 sayHelloToCat(catName: "Thomas")
-
 func sayHello(name: String) {
     print("Hello, \(name), why do you sleep so much?")
 }
